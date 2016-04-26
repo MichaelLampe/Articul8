ecbfa509b3d7fda88f3747f4c0c8cb3eb392c946
schriste92 <sjchristensen1@gmail.com>
2016-04-25 20:04:10 -0500

    reading data and speaking words based on what is coming from the bluetooth module

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,10 +8,10 @@ var dropdownSelectedButtonColor = "#32CD32";
 var buttonColor = "#d9d9d9";
 var lastindex = -1;
 var default_words = [
-    "want","ipad","help","love","colors","yes","no","bath",
-    "school","read","hug","brush teeth", "ball","music","potty","up",
-    "snack","sing","alphabet","walk","milk","mama","daddy","go",
-    "car","numbers","more","blanket","stop", "", ""
+  "want","ipad","help","love","colors","yes","no","bath",
+  "school","read","hug","brush teeth", "ball","music","potty","up",
+  "snack","sing","alphabet","walk","milk","mama","daddy","go",
+  "car","numbers","more","blanket","stop", "", ""
 ];
 // Word labels
 // Global variables
@@ -19,68 +19,73 @@ var words;
 var current_k;
 
 /*
- Highlight designate words
- */
+Highlight designate words
+*/
 highlightWord = function(){
-    var menu = document.getElementById('select_menu');
-    var button = document.getElementById('button_' + menu.selectedIndex.toString());
-    button.setAttribute('class','highlight_key');
+  var menu = document.getElementById('select_menu');
+  var button = document.getElementById('button_' + menu.selectedIndex.toString());
+  button.setAttribute('class','highlight_key');
 };
 
 
 
 displayWordLabels = function() {
-    document.getElementById("word_labels").classList.remove("hidden-word-labels");
-    document.getElementById("word_labels").classList.add("show-word-labels");
+  document.getElementById("word_labels").classList.remove("hidden-word-labels");
+  document.getElementById("word_labels").classList.add("show-word-labels");
 };
 
 hideWordLabels = function() {
-    document.getElementById("word_labels").classList.add("hidden-word-labels");
-    document.getElementById("word_labels").classList.remove("show-word-labels");
+  document.getElementById("word_labels").classList.add("hidden-word-labels");
+  document.getElementById("word_labels").classList.remove("show-word-labels");
 };
 
 /*
 TODO REIMPLEMENT
 toggleVoiceActive = function(){
-    voiceActive = !voiceActive;
-    voice_active = document.getElementById("voice-active");
-    if (voiceActive){
-        voice_active.style.background = "green";
-    } else {
-        voice_active.style.background = "red";
-    }
+voiceActive = !voiceActive;
+voice_active = document.getElementById("voice-active");
+if (voiceActive){
+voice_active.style.background = "green";
+} else {
+voice_active.style.background = "red";
+}
 };
 */
 
 
 
 /*
- Helper Functions
- */
+Helper Functions
+*/
 saveWordsToLocalStorage = function(words){
-    window.localStorage['words'] = words.toString();
+  window.localStorage['words'] = words.toString();
 };
 
 sayString = function(word) {
-    // https://forum.ionicframework.com/t/problems-with-text-to-speech/31927
-    if (voiceActive) {
-        if (window.TTS != undefined) {
-            window.TTS
-                .speak({
-                    text: word,
-                    locale: 'en-US',
-                    rate: 1.25
-                });
-        } else {
-            console.log("Unable to find Text to speech plugin");
-        }
+  if (true) {
+    if (window.TTS != undefined) {
+      console.log("trying to say " + word);
+      console.log(window.TTS);
+      window.TTS
+      .speak({
+        text: word,
+        locale: 'en-US',
+        rate: 1.25
+      }, function () {
+          alert('success');
+      }, function (reason) {
+          alert(reason);
+      });
+    } else {
+      console.log("Unable to find Text to speech plugin");
     }
+  }
 };
 
 
 
 angular.module('articulate.controllers', []).controller('SettingsCtrl', function($scope) {
-    console.log("Loaded settings controller");
+  console.log("Loaded settings controller");
 })
 
 .controller('BLECtrl', function($scope, BLE) {
@@ -88,25 +93,25 @@ angular.module('articulate.controllers', []).controller('SettingsCtrl', function
   $scope.devices = BLE.devices;
 
   var success = function () {
-      if ($scope.devices.length < 1) {
-          // a better solution would be to update a status message rather than an alert
-          alert("Didn't find any Bluetooth Low Energy devices.");
-      }
+    if ($scope.devices.length < 1) {
+      // a better solution would be to update a status message rather than an alert
+      alert("Didn't find any Bluetooth Low Energy devices.");
+    }
   };
 
   var failure = function (error) {
-      alert(error);
+    alert(error);
   };
 
   // pull to refresh
   $scope.onRefresh = function() {
-      BLE.scan().then(
-          success, failure
-      ).finally(
-          function() {
-              $scope.$broadcast('scroll.refreshComplete');
-          }
-      )
+    BLE.scan().then(
+      success, failure
+    ).finally(
+      function() {
+        $scope.$broadcast('scroll.refreshComplete');
+      }
+    )
   };
 
   // initial scan
@@ -115,155 +120,160 @@ angular.module('articulate.controllers', []).controller('SettingsCtrl', function
 
 .controller('BLEDetailCtrl', function($scope, $stateParams, BLE) {
   BLE.connect($stateParams.deviceId).then(
-      function(peripheral) {
-          $scope.device = peripheral;
-      }
+    function(peripheral) {
+      console.log("first function");
+      $scope.device = peripheral;
+    }
   );
 })
 
 
 .controller('DashCtrl',function($scope, $ionicPopup, $timeout, $compile) {
-// Containers for buttons, 4 columns
-    var c1 = document.getElementById("l_button_container");
-    var c2 = document.getElementById("ml_button_container");
-    var c3 = document.getElementById("mr_button_container");
-    var c4 = document.getElementById("r_button_container");
-    var buttonContainers = [c1, c2, c3, c4];
+  // Containers for buttons, 4 columns
+  var c1 = document.getElementById("l_button_container");
+  var c2 = document.getElementById("ml_button_container");
+  var c3 = document.getElementById("mr_button_container");
+  var c4 = document.getElementById("r_button_container");
+  var buttonContainers = [c1, c2, c3, c4];
 
-    // Check for words in local storage, otherwise use defaults.
-    if(window.localStorage['words'] === undefined){
-        console.log("Loading default word list");
-        saveWordsToLocalStorage(default_words);
-    }
-    words = window.localStorage['words'].split(",");
-    var word_labels = [];
-    for (i = 0; i < 8; i++){
-        word_label = document.getElementById("word_label_" + String(i));
-        word_labels.push(word_label)
-    }
+  // Check for words in local storage, otherwise use defaults.
+  if(window.localStorage['words'] === undefined){
+    console.log("Loading default word list");
+    saveWordsToLocalStorage(default_words);
+  }
+  words = window.localStorage['words'].split(",");
+  var word_labels = [];
+  for (i = 0; i < 8; i++){
+    word_label = document.getElementById("word_label_" + String(i));
+    word_labels.push(word_label)
+  }
 
-    /*
-     Word labels
-     */
-    modifyWordLabels = function(column) {
-        start = 8*column;
-        var fudge = 0;
-        if (column === 3){
-            fudge = 1;
-            word_labels[0].innerHTML = "";
-        }
-
-        for (i = start + fudge; i < start+8; i++){
-            message = words[i-fudge];
-            if (words[i-fudge] == undefined) {
-                message = "";
-            }
-
-            word_labels[i - start].innerHTML = message;
-        }
-    };
-
-    /*
-     Buttons
-     */
-
-    for (var i = 0; i < 31; i++){
-        // Create a given button
-        var button = document.createElement('button');
-        button.setAttribute('class', 'key');
-        button.setAttribute('id', 'button_' + i.toString());
-        button.setAttribute('value', words[i]);
-        button.setAttribute('ng-click', "showPopup($event)");
-        button.style.background = buttonColor;
-
-        // Recompile so ng-click update registers.
-        $compile(button) ($scope);
-
-        // Append to the correct div
-        buttonContainers[Math.floor(i / 8)].appendChild(button);
+  /*
+  Word labels
+  */
+  modifyWordLabels = function(column) {
+    start = 8*column;
+    var fudge = 0;
+    if (column === 3){
+      fudge = 1;
+      word_labels[0].innerHTML = "";
     }
 
-    /*
-     Display word drawers
-     */
-    displayWordsColumn = function(keep) {
-        k = parseInt(keep);
+    for (i = start + fudge; i < start+8; i++){
+      message = words[i-fudge];
+      if (words[i-fudge] == undefined) {
+        message = "";
+      }
 
-        if (showingAllColumns){
-            displayWordLabels();
-            current_k = k;
-        } else {
-            hideWordLabels();
+      word_labels[i - start].innerHTML = message;
+    }
+  };
+
+  /*
+  Buttons
+  */
+
+  for (var i = 0; i < 31; i++){
+    // Create a given button
+    var button = document.createElement('button');
+    button.setAttribute('class', 'key');
+    button.setAttribute('id', 'button_' + i.toString());
+    button.setAttribute('value', words[i]);
+    button.setAttribute('ng-click', "showPopup($event)");
+    button.style.background = buttonColor;
+
+    // Recompile so ng-click update registers.
+    $compile(button) ($scope);
+
+    // Append to the correct div
+    buttonContainers[Math.floor(i / 8)].appendChild(button);
+  }
+
+  /*
+  Display word drawers
+  */
+  displayWordsColumn = function(keep) {
+    k = parseInt(keep);
+
+    if (showingAllColumns){
+      displayWordLabels();
+      current_k = k;
+    } else {
+      hideWordLabels();
+    }
+
+    // Switch word labels to correct values
+    modifyWordLabels(k);
+    for (i = 0; i < 4; i++) {
+
+      // Add or remove all but the column that signaled.
+      if (i !== k) {
+        if (showingAllColumns) {
+          buttonContainers[i].classList.add("hidden-button-container");
+        } else
+        {
+          buttonContainers[i].classList.remove("hidden-button-container");
         }
+      }
+    }
 
-        // Switch word labels to correct values
-        modifyWordLabels(k);
-        for (i = 0; i < 4; i++) {
+    // Invert showing
+    showingAllColumns = !showingAllColumns;
+  };
 
-            // Add or remove all but the column that signaled.
-            if (i !== k) {
-                if (showingAllColumns) {
-                    buttonContainers[i].classList.add("hidden-button-container");
-                } else
-                {
-                    buttonContainers[i].classList.remove("hidden-button-container");
-                }
-            }
+  // Button action on click
+  $scope.showPopup = function($event) {
+    // Keep scope
+    $scope.data = {};
+
+    // Log these for later use when we reassign word in word array
+    $scope.data.current_button_index = $event.currentTarget.id.replace("button_", "");
+    $scope.data.current_button = $event.currentTarget;
+
+    var myPopup = $ionicPopup.show({
+      template: '<input ng-model="data.new_word" type="text" placeholder="New Word">',
+      title: 'Change Button Words',
+      subTitle: 'Current word: ' + $event.currentTarget.value,
+      scope: $scope,
+      buttons: [
+        {
+          text: '<b>Finished</b>',
+          onTap: function (e) {
+            return $scope;
+          }
         }
+      ]
+    });
 
-        // Invert showing
-        showingAllColumns = !showingAllColumns;
-    };
+    myPopup.then(function(res) {
+      // Check if there is an update
+      if (res.data.new_word) {
+        // Change word to new word and save to local storage for later use.
+        words[res.data.current_button_index] = res.data.new_word;
 
-    // Button action on click
-    $scope.showPopup = function($event) {
-        // Keep scope
-        $scope.data = {};
-
-        // Log these for later use when we reassign word in word array
-        $scope.data.current_button_index = $event.currentTarget.id.replace("button_", "");
-        $scope.data.current_button = $event.currentTarget;
-
-        var myPopup = $ionicPopup.show({
-            template: '<input ng-model="data.new_word" type="text" placeholder="New Word">',
-            title: 'Change Button Words',
-            subTitle: 'Current word: ' + $event.currentTarget.value,
-            scope: $scope,
-            buttons: [
-                {
-                    text: '<b>Finished</b>',
-                    onTap: function (e) {
-                        return $scope;
-                    }
-                }
-            ]
-        });
-
-        myPopup.then(function(res) {
-            // Check if there is an update
-            if (res.data.new_word) {
-                // Change word to new word and save to local storage for later use.
-                words[res.data.current_button_index] = res.data.new_word;
-
-                // Update button value
-                res.data.current_button.setAttribute('value', words[res.data.current_button_index]);
-                saveWordsToLocalStorage(words);
-                if (!showingAllColumns) {
-                    modifyWordLabels(current_k);
-                }
-                sayString(res.data.new_word);
-            } else{
-                // Log a cancellation.
-                console.log("No word supplied.")
-            }
-        });
-    };
+        // Update button value
+        res.data.current_button.setAttribute('value', words[res.data.current_button_index]);
+        saveWordsToLocalStorage(words);
+        if (!showingAllColumns) {
+          modifyWordLabels(current_k);
+        }
+        sayString(res.data.new_word);
+      } else{
+        // Log a cancellation.
+        console.log("No word supplied.")
+      }
+    });
+  };
 })
 
 .controller('BLEDetailCtrl', function($scope, $stateParams, BLE) {
-    BLE.connect($stateParams.deviceId).then(
-        function(peripheral) {
-            $scope.device = peripheral;
-        }
-    );
+  BLE.connect($stateParams.deviceId).then(
+    function(peripheral) {
+      BLE.subscribe($stateParams.deviceId, function(data){
+        console.log("it returned " + data);
+        sayString(words[data]);
+      });
+      //$scope.device = peripheral;
+    }
+  );
 })
--- www/js/services.js
+++ www/js/services.js
@@ -4,75 +4,79 @@ angular.module('articulate.services', [])
 
   var connected;
 
+  // ASCII only
+  function bytesToString(buffer) {
+    return String.fromCharCode.apply(null, new Uint8Array(buffer));
+  }
+
   return {
 
     devices: [],
 
     scan: function() {
-        var that = this;
-        var deferred = $q.defer();
+      var that = this;
+      var deferred = $q.defer();
 
-        that.devices.length = 0;
+      that.devices.length = 0;
 
-        // disconnect the connected device (hack, device should disconnect when leaving detail page)
-        if (connected) {
-            var id = connected.id;
-            ble.disconnect(connected.id, function() {
-                console.log("Disconnected " + id);
-            });
-            connected = null;
-        }
+      // disconnect the connected device (hack, device should disconnect when leaving detail page)
+      if (connected) {
+        var id = connected.id;
+        ble.disconnect(connected.id, function() {
+          console.log("Disconnected " + id);
+        });
+        connected = null;
+      }
 
-        ble.startScan([],  /* scan for all services */
-            function(peripheral){
-                that.devices.push(peripheral);
-            },
-            function(error){
-                deferred.reject(error);
-            });
+      ble.startScan([],  /* scan for all services */
+        function(peripheral){
+          that.devices.push(peripheral);
+        },
+        function(error){
+          deferred.reject(error);
+        });
 
         // stop scan after 5 seconds
         setTimeout(ble.stopScan, 5000,
-            function() {
-                deferred.resolve();
-            },
-            function() {
-                console.log("stopScan failed");
-                deferred.reject("Error stopping scan");
-            }
+          function() {
+            deferred.resolve();
+          },
+          function() {
+            console.log("stopScan failed");
+            deferred.reject("Error stopping scan");
+          }
         );
 
         return deferred.promise;
-    },
-    connect: function(deviceId) {
+      },
+      connect: function(deviceId) {
         var deferred = $q.defer();
 
         ble.connect(deviceId,
-            function(peripheral) {
-                connected = peripheral;
-                deferred.resolve(peripheral);
-            },
-            function(reason) {
-                deferred.reject(reason);
-            }
+          function(peripheral) {
+            connected = peripheral;
+            deferred.resolve(peripheral);
+          },
+          function(reason) {
+            deferred.reject(reason);
+          }
         );
 
         return deferred.promise;
-    },
-    subscribe: function(deviceId) {
-        var deferred = $q.defer();
-
-        ble.connect(deviceId,
-            function(peripheral) {
-                connected = peripheral;
-                deferred.resolve(peripheral);
-            },
-            function(reason) {
-                deferred.reject(reason);
-            }
+      },
+      subscribe: function(deviceId, callback) {
+        ble.startNotification(deviceId,
+          "6e400001-b5a3-f393-e0a9-e50e24dcca9e",
+          "6e400003-b5a3-f393-e0a9-e50e24dcca9e",
+          function(data) {
+            console.log(bytesToString(data));
+            callback(bytesToString(data));
+          },
+          function(reason) {
+            console.log("ERROR: " + reason);
+          }
         );
-
         return deferred.promise;
-    }
-  };
-});
+      }
+    };
+  });
